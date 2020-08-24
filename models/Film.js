70ffeb9a21b17430fa55bf7b18c718b09c176b84
Teresa Pelinski <teresapelinski@gmail.com>
2020-08-24 23:13:59 +0200

    changed default:unknown to default:undefined

--- models/Film.js
+++ models/Film.js
@@ -6,12 +6,12 @@ const schema = new Schema(
     serialNumber: { type: String, unique: true, required: true },
     format: { type: String, default: "35mm" },
     filmType: { type: String, required: true, default: undefined },
-    camera: { type: String, default: "unknown" },
-    colorType: { type: String, enum: ["b/w", "color"], default: "unknown" },
-    scan: { type: String, enum: ["CD", "scan", "none"], default: "none" },
+    camera: { type: String, default: undefined },
+    colorType: { type: String, enum: ["b/w", "color"], default: undefined },
+    scan: { type: String, enum: ["CD", "scan", "none"], default: undefined },
     date: {
       year: { type: Number, default: undefined },
-      month: [{ type: String, enum: ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"], default: "unknown" }]
+      month: [{ type: String, enum: ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"], default: undefined }]
     },
     location: [{ type: String, default: undefined }],
     comments: { type: String, default: null }
